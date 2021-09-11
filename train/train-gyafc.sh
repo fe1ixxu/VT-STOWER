213350f45165fbc8756244a636f255d27af18721
Haoran Xu <45837851+fe1ixxu@users.noreply.github.com>
2021-09-10 21:55:51 -0400

    Update train-gyafc.sh

--- train/train-gyafc.sh
+++ train/train-gyafc.sh
@@ -23,4 +23,4 @@ CUDA_VISIBLE_DEVICES=0 fairseq-train ${TEXT}databin-01/ --arch vae --ddp-backend
 --lr 0.0005 --lr-scheduler inverse_sqrt  --dropout 0.1 --warmup-updates 1000 --warmup-init-lr 1e-07 --weight-decay 0.0 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
 --max-tokens 8192 --update-freq 3 --attention-dropout 0.1 --activation-dropout 0.1 --max-update 100000 --save-dir ${SAVE_DIR}stage2  --encoder-embed-dim 768 --decoder-embed-dim 768 --max-epoch 600  \
 --save-interval 1  --alpha 1 --no-epoch-checkpoints  --weight_c 1  --pretrained_model roberta-base --vae_encoder_layers 1 --vae_type base --transfer_task ${TASK} \
---warmup_from_nmt --warmup_nmt_file ${SAVE_DIR}stage1/checkpoint_best.pt --reset-lr-scheduler --reset-optimizer --stage 1 ${SAVE_DIR}classifier/pytorch_model.bin
+--warmup_from_nmt --warmup_nmt_file ${SAVE_DIR}stage1/checkpoint_best.pt --reset-lr-scheduler --reset-optimizer --stage 1 --score_maker ${SAVE_DIR}classifier/pytorch_model.bin
